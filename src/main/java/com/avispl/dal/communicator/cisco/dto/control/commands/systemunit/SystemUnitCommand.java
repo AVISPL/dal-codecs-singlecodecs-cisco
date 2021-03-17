7fc0eaacf2a13758e36476498db675cdc73ffa57
Maksym Rossiitsev <maksym.rossiitsev@avispl.com>
2021-03-17 20:24:10 +0200

    SYAL-180: Add SystemUnitCommand.java

---
+++ src/main/java/com/avispl/dal/communicator/cisco/dto/control/commands/systemunit/SystemUnitCommand.java
@@ -0,0 +1,35 @@
+package com.avispl.dal.communicator.cisco.dto.control.commands.systemunit;
+
+import javax.xml.bind.annotation.XmlAccessType;
+import javax.xml.bind.annotation.XmlAccessorType;
+import javax.xml.bind.annotation.XmlAttribute;
+import javax.xml.bind.annotation.XmlElement;
+
+@XmlAccessorType(XmlAccessType.NONE)
+public class SystemUnitCommand {
+    public static enum BootAction {Restart}
+    @XmlAccessorType(XmlAccessType.NONE)
+    public static class Boot {
+        @XmlAttribute
+        private String command = "True";
+        @XmlElement(name = "Action")
+        private String action;
+
+        public Boot(){}
+        public Boot(BootAction action) {
+            this.action = action.name();
+        }
+
+        public String getCommand() { return command; }
+
+        public String getAction() { return action; }
+    }
+    @XmlElement(name = "Boot")
+    private Boot boot;
+    public SystemUnitCommand() {}
+    public SystemUnitCommand(BootAction action) {
+        boot = new Boot(action);
+    }
+
+    public Boot getBoot() { return boot; }
+}

23c9e0f80cb3e65a63b05d510b4e9ae8b99f5a23
Maksym.Rossiytsev <maksym.rossiitsev@avispl.com>
2022-10-03 18:33:23 +0300

    SYAL-1069 Invalid error handling in Cisco single codec adapter

--- src/main/java/com/avispl/dal/communicator/cisco/statistics/DynamicStatisticsDefinitions.java
+++ src/main/java/com/avispl/dal/communicator/cisco/statistics/DynamicStatisticsDefinitions.java
@@ -13,13 +13,13 @@ import java.util.Optional;
  * @since 1.2.0
  * */
 public enum DynamicStatisticsDefinitions {
-    ActiveCallsNumber("SystemUnit#ActiveCallsNumber"),
-    SuspendedCallsNumber("SystemUnit#SuspendedCallsNumber"),
-    InProgressCallsNumber("SystemUnit#InProgressCallsNumber"),
-    HardwareTemperature("SystemUnit#HardwareTemperature(C)"),
-    CurrentPeopleCount("RoomAnalytics#CurrentPeopleCount"),
-    SoundLevelA("RoomAnalytics#SoundLevelA"),
-    AmbientNoiseLevelA("RoomAnalytics#AmbientNoiseLevelA");
+    ActiveCallsNumber("ActiveCallsNumber"),
+    SuspendedCallsNumber("SuspendedCallsNumber"),
+    InProgressCallsNumber("InProgressCallsNumber"),
+    HardwareTemperature("HardwareTemperature(C)"),
+    CurrentPeopleCount("CurrentPeopleCount"),
+    SoundLevelA("SoundLevelA"),
+    AmbientNoiseLevelA("AmbientNoiseLevelA");
 
     private final String name;
     DynamicStatisticsDefinitions(final String name) {
@@ -42,7 +42,7 @@ public enum DynamicStatisticsDefinitions {
      * @return true if definition exists, false otherwise
      * */
     public static boolean checkIfExists(String name) {
-        Optional<DynamicStatisticsDefinitions> dynamicStatisticsProperty = Arrays.stream(DynamicStatisticsDefinitions.values()).filter(c -> name.contains(c.getName())).findFirst();
+        Optional<DynamicStatisticsDefinitions> dynamicStatisticsProperty = Arrays.stream(DynamicStatisticsDefinitions.values()).filter(c -> name.endsWith(c.getName())).findFirst();
         if (dynamicStatisticsProperty.isPresent()) {
             return true;
         }

d55c4d9950755d17ad8df04b52fb186dc15dcc70
MaksimR-AVISPL <maksym.rossiitsev@avispl.com>
2024-08-05 19:57:39 +0300

    SYAL-1700 Cisco XMLAPI Adapter - v1.1.4 Fixes and Adjustments

--- src/main/java/com/avispl/dal/communicator/cisco/dto/status/teams/MicrosoftTeamsHardwareAcceleratorEncoder.java
+++ src/main/java/com/avispl/dal/communicator/cisco/dto/status/teams/MicrosoftTeamsHardwareAcceleratorEncoder.java
@@ -8,6 +8,16 @@ import com.fasterxml.jackson.dataformat.xml.annotation.JacksonXmlProperty;
 public class MicrosoftTeamsHardwareAcceleratorEncoder extends MicrosoftTeamsHardwareAcceleratorMetrics {
     @JacksonXmlProperty(localName = "InputMode")
     private String inputMode;
+    @JacksonXmlProperty(isAttribute = true, localName="item")
+    private String item;
+
+    public String getItem() {
+        return item;
+    }
+
+    public void setItem(String item) {
+        this.item = item;
+    }
 
     /**
      * Retrieves {@link #inputMode}

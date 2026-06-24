3a63d8aa15b6bfd0ca62ac64b865700c69ea9d1b
RitikM-AVISPL <ritik.madaan@avispl.com>
2026-06-24 21:16:37 +0530

    Updated 'FarEndControl' property to be a dropdown

--- src/main/java/com/avispl/dal/communicator/cisco/CiscoCommunicator.java
+++ src/main/java/com/avispl/dal/communicator/cisco/CiscoCommunicator.java
@@ -1724,7 +1724,7 @@ public class CiscoCommunicator extends RestCommunicator implements CallControlle
 
             ConferenceConfigurationFarEndControl farEndControlConfiguration = conferenceConfiguration.getFarEndControl();
             if (farEndControlConfiguration != null) {
-                addStatisticsParameterWithDropdown(statistics, controls, CONFERENCE_FAR_END_CONTROL, Arrays.asList("On", "Off", "Auto"), farEndControlConfiguration.getMode().getValue());
+                addStatisticsParameterWithDropdown(statistics, controls, CONFERENCE_FAR_END_CONTROL, farEndControlConfiguration.getMode(), valuespace);
             }
         }
     }
